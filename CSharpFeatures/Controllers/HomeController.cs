288d5a5e22dcf507725713e156f97f790bd7052a
Pedro Melo <pedroph.alves@hotmail.com>
2018-03-07 12:42:05 -0300

    first commit

---
+++ CSharpFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CSharpFeatures.Models;
+
+namespace CSharpFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+
+            myProduct.Name = "Peter";
+
+            string productName = myProduct.Name;
+
+            return View("Result", (object) String.Format("Product Name: {0}", productName));
+        }
+    }
+}
---
+++ CSharpFeatures/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharpFeatures.MvcApplication" Language="C#" %>
