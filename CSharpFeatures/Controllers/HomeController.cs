92c99a8886e886b66c9f55b18d61ac24ba5a0f02
Pedro Melo <pedroph.alves@hotmail.com>
2018-03-07 15:15:26 -0300

    Overview and examples of features of C#

--- CSharpFeatures/Controllers/HomeController.cs
+++ CSharpFeatures/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using CSharpFeatures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
-using CSharpFeatures.Models;
 
 namespace CSharpFeatures.Controllers
 {
@@ -23,7 +24,172 @@ namespace CSharpFeatures.Controllers
 
             string productName = myProduct.Name;
 
-            return View("Result", (object) String.Format("Product Name: {0}", productName));
+            return View("Result", (object)String.Format("Product Name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result",
+            (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int> {
+                { "apple", 10 },
+                { "orange", 20 },
+                { "plum", 30 }
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
+
+        public ViewResult UseExtension()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                     new Product {Name = "Kayak", Price = 275M},
+                     new Product {Name = "Lifejacket", Price = 48.95M},
+                     new Product {Name = "Soccer ball", Price = 19.50M},
+                     new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+            
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result",
+            (object)String.Format("Total: {0:c}", cartTotal));
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                     new Product {Name = "Kayak", Price = 275M},
+                     new Product {Name = "Lifejacket", Price = 48.95M},
+                     new Product {Name = "Soccer ball", Price = 19.50M},
+                     new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+
+            Product[] productArray = {
+                 new Product {Name = "Kayak", Price = 275M},
+                 new Product {Name = "Lifejacket", Price = 48.95M},
+                 new Product {Name = "Soccer ball", Price = 19.50M},
+                 new Product {Name = "Corner flag", Price = 34.95M}
+             };
+
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
+
+            return View("Result",
+            (object)String.Format("Cart Total: {0}, Array Total: {1}",
+            cartTotal, arrayTotal));
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product> {
+                    new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                    new Product {Name = "Lifejacket", Category = "Watersports",
+                    Price = 48.95M},
+                    new Product {Name = "Soccer ball", Category = "Soccer",
+                    Price = 19.50M},
+                    new Product {Name = "Corner flag", Category = "Soccer",
+                    Price = 34.95M}
+                }
+            };
+
+            decimal total = 0;
+            foreach (Product prod in products.Filter(prod => prod.Category == "Soccer"))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
+
+        public ViewResult CreateAnonArray()
+        {
+            var oddsAndEnds = new[] {
+                new { Name = "MVC", Category = "Pattern"},
+                new { Name = "Hat", Category = "Clothing"},
+                new { Name = "Apple", Category = "Fruit"}
+            };
+            StringBuilder result = new StringBuilder();
+            foreach (var item in oddsAndEnds)
+            {
+                result.Append(item.Name).Append(" ");
+            }
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult FindProducts()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var foundProducts = products
+                .OrderByDescending(p => p.Price)
+                .Take(3)
+                .Select(p => new { Name = p.Name, Price = p.Price});
+
+            products[2] = new Product { Name = "Stadium", Price = 79600M };
+
+            //var foundProducts = from match in products
+            //                    orderby match.Price descending
+            //                    select new { match.Name, match.Price };
+
+            //int count = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+                //if(++count == 3)
+                //{
+                //    break;
+                //}
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult SumProducts()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var results = products.Sum(e => e.Price);
+
+            products[2] = new Product { Name = "Stadium", Price = 79500M };
+
+            return View("Result",
+            (object)String.Format("Sum: {0:c}", results));
         }
     }
 }
---
+++ CSharpFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cart)
+        {
+            decimal total = 0;
+
+            foreach (Product p in cart.Products)
+            {
+                total += p.Price;
+            }
+
+            return total;
+        }
+
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+
+            foreach (Product p in products)
+            {
+                total += p.Price;
+            }
+
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByCategory( this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> Filter( this IEnumerable<Product> productEnum, Func<Product, bool> selectorParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (selectorParam(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+    }
+}
